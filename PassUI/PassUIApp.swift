f0dd6cdf8a5827ef3d4edb666b44242acc0d7e2c
Ge Will <531sunlight@gmail.com>
2023-07-11 17:07:59 +0800

    Initial Commit

---
+++ PassUI/PassUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassUIApp.swift
+//  PassUI
+//
+//  Created by will on 11/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct PassUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
